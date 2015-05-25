0e8ad7cd272129b8b4288b65a70ab8a5b0dcbd67
nandha-dev <nandha21.2@gmail.com>
2015-05-25 17:51:13 +0530

    player model and JSON Parsed files added

---
+++ Meteor/src/com/nandha/meteorLeaderBoard/models/Player.java
@@ -0,0 +1,48 @@
+package com.nandha.meteorLeaderBoard.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Player {
+
+	private String id;
+	private String name;
+	private int score;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	// json parser
+	public void jsonParser(JSONObject playerJsonObject, String id) {
+		try {
+			this.setId(id);
+			this.setName(playerJsonObject.getString("name"));
+			this.setScore(playerJsonObject.getInt("score"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
